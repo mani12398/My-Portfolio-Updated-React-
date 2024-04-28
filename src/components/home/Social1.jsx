3e85bfbf464ec4ac39c2afebaf01cf5ebd29eb44
Muhammad Abdur-Rehman <muhammadabdurrehman516@gmail.com>
2024-04-28 23:48:30 +0500

    Some Changes

--- src/components/home/Social1.jsx
+++ src/components/home/Social1.jsx
@@ -3,17 +3,17 @@ import React from "react";
 export const Social1 = () => {
     return (
         <div className="home__social">
-            <a href="https://www.linkedin.com/in/muhammad-abdur-rehman-fs20bscs/" target="_blank" className="home__social-icon">
+            <a href="https://www.linkedin.com/in/muhammad-abdur-rehman-fs20bscs/" target="_blank" rel="noreferrer" className="home__social-icon">
                 <i class="uil uil-linkedin"></i>
             </a>
 
             <a
-                href="https://github.com/mani12398" target="_blank" className="home__social-icon">
+                href="https://github.com/mani12398" target="_blank" rel="noreferrer" className="home__social-icon">
                 <i class="uil uil-github"></i>
             </a>
 
             <a
-                href="https://www.instagram.com/ar_rehman021/" target="__blank" className="home__social-icon">
+                href="https://www.instagram.com/ar_rehman021/" target="__blank" rel="noreferrer" className="home__social-icon">
                 <i class="uil uil-instagram"></i>
             </a>
         </div>
