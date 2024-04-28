5c9920e6931d37d3d017d988b553a49daafae26a
Muhammad Abdur-Rehman <muhammadabdurrehman516@gmail.com>
2024-04-28 22:38:00 +0500

    Some Updates

--- src/components/home/Home1.jsx
+++ src/components/home/Home1.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import "./home1.css";
 import Social1 from './Social1';
+import Data1 from './Data1';
+import ScrollDown1 from './ScrollDown1';
 
 const Home1 = () => {
   return (
@@ -9,8 +11,9 @@ const Home1 = () => {
             <div className="home__content grid">
                 <Social1/>
                 <div className="home__img"></div>
-                <Data/>
+                <Data1/>
             </div>
+            <ScrollDown1/>
         </div>
     </section>
   )
