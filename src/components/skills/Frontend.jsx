8b9230fb018299bf62ebdd9dd96b2714ce5d5d23
Muhammad Abdur-Rehman <muhammadabdurrehman516@gmail.com>
2024-06-23 13:13:28 +0500

    Some Changes

--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -10,7 +10,7 @@ const Frontend = () => {
                 <div className="skills__data">
                     <i className='bx bx-badge-check'></i>
                     <div>
-                        <h3 className="skills__name"></h3>
+                        <h3 className="skills__name">HTML</h3>
                         <span className="skills__level"></span>
                     </div>
                 </div>
