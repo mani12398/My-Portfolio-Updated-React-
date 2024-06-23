873f017275e2b2e0d19b2f8bbab716945df6904f
Muhammad Abdur-Rehman <muhammadabdurrehman516@gmail.com>
2024-06-23 16:50:37 +0500

    Some Updates

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -10,24 +10,24 @@ const Backend = () => {
                 <div className="skills__data">
                     <i className='bx bx-badge-check'></i>
                     <div>
-                        <h3 className="skills__name"></h3>
-                        <span className="skills__level"></span>
+                        <h3 className="skills__name">HTML</h3>
+                        <span className="skills__level">HTML</span>
                     </div>
                 </div>
 
                 <div className="skills__data">
                     <i className='bx bx-badge-check'></i>
                     <div>
-                        <h3 className="skills__name"></h3>
-                        <span className="skills__level"></span>
+                        <h3 className="skills__name">HTML</h3>
+                        <span className="skills__level">HTML</span>
                     </div>
                 </div>
 
                 <div className="skills__data">
                     <i className='bx bx-badge-check'></i>
                     <div>
-                        <h3 className="skills__name"></h3>
-                        <span className="skills__level"></span>
+                        <h3 className="skills__name">HTML</h3>
+                        <span className="skills__level">HTML</span>
                     </div>
                 </div>
 
@@ -38,24 +38,24 @@ const Backend = () => {
                 <div className="skills__data">
                     <i className='bx bx-badge-check'></i>
                     <div>
-                        <h3 className="skills__name"></h3>
-                        <span className="skills__level"></span>
+                        <h3 className="skills__name">HTML</h3>
+                        <span className="skills__level">HTML</span>
                     </div>
                 </div>
 
                 <div className="skills__data">
                     <i className='bx bx-badge-check'></i>
                     <div>
-                        <h3 className="skills__name"></h3>
-                        <span className="skills__level"></span>
+                        <h3 className="skills__name">HTML</h3>
+                        <span className="skills__level">HTML</span>
                     </div>
                 </div>
 
                 <div className="skills__data">
                     <i className='bx bx-badge-check'></i>
                     <div>
-                        <h3 className="skills__name"></h3>
-                        <span className="skills__level"></span>
+                        <h3 className="skills__name">HTML</h3>
+                        <span className="skills__level">HTML</span>
                     </div>
                 </div>
                 
--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -18,16 +18,16 @@ const Frontend = () => {
                 <div className="skills__data">
                     <i className='bx bx-badge-check'></i>
                     <div>
-                        <h3 className="skills__name"></h3>
-                        <span className="skills__level"></span>
+                        <h3 className="skills__name">HTML</h3>
+                        <span className="skills__level">HTML</span>
                     </div>
                 </div>
 
                 <div className="skills__data">
                     <i className='bx bx-badge-check'></i>
                     <div>
-                        <h3 className="skills__name"></h3>
-                        <span className="skills__level"></span>
+                        <h3 className="skills__name">HTML</h3>
+                        <span className="skills__level">HTML</span>
                     </div>
                 </div>
 
@@ -38,24 +38,24 @@ const Frontend = () => {
                 <div className="skills__data">
                     <i className='bx bx-badge-check'></i>
                     <div>
-                        <h3 className="skills__name"></h3>
-                        <span className="skills__level"></span>
+                        <h3 className="skills__name">HTML</h3>
+                        <span className="skills__level">HTML</span>
                     </div>
                 </div>
 
                 <div className="skills__data">
                     <i className='bx bx-badge-check'></i>
                     <div>
-                        <h3 className="skills__name"></h3>
-                        <span className="skills__level"></span>
+                        <h3 className="skills__name">HTML</h3>
+                        <span className="skills__level">HTML</span>
                     </div>
                 </div>
 
                 <div className="skills__data">
                     <i className='bx bx-badge-check'></i>
                     <div>
-                        <h3 className="skills__name"></h3>
-                        <span className="skills__level"></span>
+                        <h3 className="skills__name">HTML</h3>
+                        <span className="skills__level">HTML</span>
                     </div>
                 </div>
                 
