5971f71c7fc0bd90e34b8360d426046fbcc4ecaf
Muhammad Abdur Rehman <muhammadabdurrehman516@gmail.com>
2024-08-24 14:11:06 -0700

    Some Updates

--- src/components/skills/ProgrammingLanguages.jsx
+++ src/components/skills/ProgrammingLanguages.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
-import {
-   
-    DiHtml5,
-   
-} from 'react-icons/di';
+import { DiHtml5 } from 'react-icons/di';
 
 const ProgrammingLanguages = () => {
     return (
